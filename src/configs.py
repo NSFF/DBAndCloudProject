f5338fc26970815881da43dfdf68c1d23f83e081
RobinVanCraenenbroek <robin.van.craenenbroek@intern.ml6.eu>
2023-08-19 19:28:24 +0200

    fixed github env var

--- src/configs.py
+++ src/configs.py
@@ -11,7 +11,5 @@ class DBConfigs:
         DB_NAME (str): Name of the database of MongoDB
     """
 
-    # os.environ["DB_READ_USERNAME"] = "read_service_account"
-    # os.environ["DB_READ_USER_PASS"] = "test123"
     DB_URI = f"mongodb+srv://{os.environ['DB_READ_USERNAME']}:{os.environ['DB_READ_USER_PASS']}@dbandcloud.s1vklr4.mongodb.net/?retryWrites=true&w=majority"
     DB_NAME = "DBAndCloud"
